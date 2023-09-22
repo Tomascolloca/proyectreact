1ad9abf7ed7800dbbb7823fbdc994d3dc7140b26
Tomas GOMEZ Colloca <tomycollo24@gmail.com>
2023-09-22 20:14:40 -0300

    sexto commit

--- src/components/PaymentForm.jsx
+++ src/components/PaymentForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './paymentform.css';
 
 const PaymentForm = () => {
   const handleCheckout = () => {
@@ -8,32 +9,67 @@ const PaymentForm = () => {
 
   return (
     <div>
-      <h2>Detalles de Pago</h2>
-      <form>
-        <div>
-          <label htmlFor="nombre">Nombre:</label>
-          <input type="text" id="nombre" placeholder="Ingrese su nombre" />
+      <h2 className="payment-heading">Detalles de Pago</h2>
+      <form className="payment-form">
+        <div className="input-container">
+          <label htmlFor="nombre" className="label">
+            Nombre:
+          </label>
+          <input
+            type="text"
+            id="nombre"
+            placeholder="Ingrese su nombre"
+            className="input"
+          />
         </div>
-        <div>
-          <label htmlFor="apellido">Apellido:</label>
-          <input type="text" id="apellido" placeholder="Ingrese su apellido" />
+        <div className="input-container">
+          <label htmlFor="apellido" className="label">
+            Apellido:
+          </label>
+          <input
+            type="text"
+            id="apellido"
+            placeholder="Ingrese su apellido"
+            className="input"
+          />
         </div>
-        {/* Agregar más campos de entrada según sea necesario */}
+        <div className="input-container">
+          <label htmlFor="email" className="label">
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Ingrese su email"
+            className="input"
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="direccion" className="label">
+            Dirección de Envío:
+          </label>
+          <input
+            type="text"
+            id="direccion"
+            placeholder="Ingrese su dirección de envío"
+            className="input"
+          />
+        </div>
       </form>
       <h3>Forma de Pago:</h3>
-      <div>
-        <label>
+      <div className="payment-methods">
+        <label className="radio-label">
           <input type="radio" name="formaPago" value="efectivo" />
           Efectivo
         </label>
-      </div>
-      <div>
-        <label>
+        <label className="radio-label">
           <input type="radio" name="formaPago" value="tarjeta" />
           Tarjeta de Crédito
         </label>
       </div>
-      <button onClick={handleCheckout}>Finalizar Compra</button>
+      <button onClick={handleCheckout} className="checkout-button">
+        Finalizar Compra
+      </button>
     </div>
   );
 };
